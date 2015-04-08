7da75242dc78213c23468fdbc43b94e14cc0bf57
Iuri Fernandes <iurifq@gmail.com>
2015-04-08 10:24:56 +0200

    Comment out raising errors from the client.
    We are dealing with the errors in the application after parsing.

--- lib/usps/client.rb
+++ lib/usps/client.rb
@@ -19,15 +19,15 @@ module USPS
       xml = Nokogiri::XML.parse(response.body)
 
       # Process and raise errors
-      if((error = xml.search('Error')).any?)
-        # This is where things get a little tricky. There are a bunch of errors
-        # that the USPS can send back.
-        why = error.search('Description').text
-        code = error.search('Number').text
-        source = error.search('Source').text
+      #if((error = xml.search('Error')).any?)
+        ## This is where things get a little tricky. There are a bunch of errors
+        ## that the USPS can send back.
+        #why = error.search('Description').text
+        #code = error.search('Number').text
+        #source = error.search('Source').text
 
-        raise Error.for_code(code).new(why, code, source)
-      end
+        #raise Error.for_code(code).new(why, code, source)
+      #end
 
       # Initialize the proper response object and parse the message
       request.response_for(xml)
